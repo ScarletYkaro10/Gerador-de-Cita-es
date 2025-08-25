b20406a452256a590dc6ed5f65702cd5d9f4b5cd
Gabriel <gabrielykaro10@gmail.com>
2025-08-25 16:38:55 -0300

    feat: Adiciona mais citações à lista

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const citacoes = ["A imaginação é mais importante que o conhecimento. - Albert Einstein",]; 
-// Lógica para gerar citação será adicionada depois.
+const citacoes = ["A imaginação é mais importante que o conhecimento. - Albert Einstein", "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
+    "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"];
